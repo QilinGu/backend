db388521e817b95adccd6bde128b859f47605674
Mark Fajet <markfajet@gmail.com>
2016-10-22 23:03:00 -0400

    More styling

--- MapRenderer.py
+++ MapRenderer.py
@@ -20,7 +20,7 @@ key = open('config.txt').read()
 def map_render(location, orientation):
     lat  = location['latitude']
     lon = location['longitude']
-    URL = "https://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=15&size=640x640&style=feature:landscape.man_made|color:0xa9a9a9&style=feature:all|element:labels|visibility:off&style=feature:road|color:0x654321&style=feature:poi.business|color:0x186842&style=feature:poi.attraction|color:0xff0000&style=feature:poi.government|color:0x00ff00&style=feature:poi.medical|color:0x0000ff&style=feature:poi.park|color:0x000000&style=feature:poi.place_of_worship|color:0x631355&style=feature:poi.school|color:0x584788&style=feature:poi.sports_complex|color:0x797923&key=%s" % (lat, lon, key)
+    URL = "https://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=15&size=640x640&maptype=terrain&style=feature:landscape.man_made|color:0x222211&style=feature:all|element:labels|visibility:off&style=feature:road|color:0x654321&style=feature:poi.business|color:0x186842&style=feature:poi.attraction|color:0xff0000&style=feature:poi.government|color:0x00ff00&style=feature:poi.medical|color:0x0000ff&style=feature:poi.park|color:0x000000&style=feature:poi.place_of_worship|color:0x631355&style=feature:poi.school|color:0x584788&style=feature:poi.sports_complex|color:0x797923&key=%s" % (lat, lon, key)
     response = requests.get(URL)
     print URL
     img = Image.open(StringIO(response.content))
