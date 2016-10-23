2cd7d346a3dc04bb0b56a74484bb52f5127abcaa
Mark Fajet <markfajet@gmail.com>
2016-10-23 00:55:26 -0400

    Minor changes. Images saved in new directory with timestamp

--- MapRenderer.py
+++ MapRenderer.py
@@ -15,17 +15,26 @@ import urllib
 from PIL import Image
 import requests
 from StringIO import StringIO
+import time
 
 key = open('config.txt').read()
-def map_render(location, orientation):
+def map_render(location):
+    #Get latitude, longitude from location dict
     lat  = location['latitude']
     lon = location['longitude']
+
+    #Get and style static google map from api
     URL = "https://maps.googleapis.com/maps/api/staticmap?center=%f,%f&zoom=16&size=640x640&maptype=terrain&style=feature:landscape.man_made|color:0x222211&style=feature:all|element:labels|visibility:off&style=feature:road|color:0x654321&style=feature:poi.business|color:0x186842&style=feature:poi.attraction|color:0xff0000&style=feature:poi.government|color:0x00ff00&style=feature:poi.medical|color:0x0000ff&style=feature:poi.park|color:0x000000&style=feature:poi.place_of_worship|color:0x631355&style=feature:poi.school|color:0x584788&style=feature:poi.sports_complex|color:0x797923&key=%s" % (lat, lon, key)
+    #Save as image file
     response = requests.get(URL)
     print URL
     img = Image.open(StringIO(response.content))
-    img = img.crop((0,0,640,615)).rotate( orientation, expand=1 )
-    img.save("map.png")
+    img = img.crop((0,0,640,615))
+    t = time.time()
+    time_string = "%f" % t
+    mult = len(time_string.split('.')[1])
+    map_name = "./maps/map_%d.png" % (t * 10**mult)
+    img.save(map_name)
 
 def update_map(where , prev):
     add_lon = 0
@@ -60,7 +69,11 @@ def map_render2(location):
     print URL
     img = Image.open(StringIO(response.content))
     img = img.crop((0,0,640,615))
-    img.save("map2.png")
+    t = time.time()
+    time_string = "%f" % t
+    mult = len(time_string.split('.')[1])
+    map_name = "./maps/map2_%d.png" % (t * 10**mult)
+    img.save(map_name)
 
 def calc_distance_degrees(distance,zoom):
     print (1.0*distance/256)*(360.0 / 2**zoom)
@@ -68,6 +81,6 @@ def calc_distance_degrees(distance,zoom):
 
 
 loc = {'latitude': 25.7721638,'longitude': -80.2182310}
-map_render(loc, 0)
+map_render(loc)
 #map_render2({'latitude': (25.7721638),'longitude': (-80.2182310 - calc_distance_degrees(640,16))}, 0)
-update_map("left", loc)
+# update_map("left", loc)
